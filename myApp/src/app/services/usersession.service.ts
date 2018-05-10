9042646eb7c3f3e120e3b2201e573125c4794a69
vamsi krishna <vamsim@twinesoft.com>
2018-05-10 05:32:48 +0530

    facebook & microsoft added jwt

--- myApp/src/app/services/usersession.service.ts
+++ myApp/src/app/services/usersession.service.ts
@@ -22,21 +22,60 @@ export class UserSessionService {
 
     if (info.providerName === 'google') {
       var gtoken = info.token;
+
+      // sending access_token to server to validate and create jwt
       var xhr = new XMLHttpRequest();
       xhr.open('POST', 'http://localhost:3000/api/auth/google');
       xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-      xhr.setRequestHeader('token', gtoken);
+      xhr.setRequestHeader('tokeng', gtoken);
       xhr.send();
 
-      this.http.get('http://localhost:3000/gjwt').subscribe(res => {(this.jwt = res);
-      console.log(this.jwt);
+      // get JWT from server
+      this.http.get('http://localhost:3000/gjwt').subscribe(res => {
+        (this.jwt = JSON.stringify(res));
+        console.log(this.jwt);
       });
     }
-    //to do : call node server and get the jwt
+
+    else if (info.providerName === 'facebook') {
+      var fbtoken = info.token;
+      console.log('facebook-token from service ====== ', fbtoken);
+
+      // sending access_token to server to validate and create jwt
+      var xhr = new XMLHttpRequest();
+      xhr.open('POST', 'http://localhost:3000/api/auth/facebook');
+      xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+      xhr.setRequestHeader('tokenf', fbtoken);
+      xhr.send();
+
+      // get JWT from server
+      this.http.get('http://localhost:3000/fbjwt').subscribe(res => {
+        (this.jwt = JSON.stringify(res));
+        console.log(this.jwt);
+      });
+    }
+
+    else if (info.providerName === 'microsoft') {
+      var mstoken = info.token;
+      console.log('microsoft-token from service ====== ', mstoken);
+
+      // sending access_token to server to validate and create jwt
+      var xhr = new XMLHttpRequest();
+      xhr.open('POST', 'http://localhost:3000/api/auth/microsoft');
+      xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+      xhr.setRequestHeader('tokenm', mstoken);
+      xhr.send();
+
+      // get JWT from server
+      // this.http.get('http://localhost:3000/msjwt').subscribe(res => {
+      //   (this.jwt = JSON.stringify(res));
+      //   console.log(this.jwt);
+      // });
+    }
   }
 }
 
-export interface IUserInfo {
+export class IUserInfo {
   firstName: string;
   lastName: string;
   email: string;
