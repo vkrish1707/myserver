0bec4e2f7511c00d03d66375050e3b04554e02f7
pavanraj <pavanr@twinesoft.com>
2018-06-09 21:25:37 +0530

    env variable created

--- mySrv/config/config.development.js
+++ mySrv/config/config.development.js
@@ -1,9 +1,3 @@
-// exports.secret = {
-//     google_client_id: '284779082637-o4uhhhiirkb7j89r8qd0jfkfmddnmq94.apps.googleusercontent.com',
-//     facebook_path: 'https://graph.facebook.com/me?access_token=',
-//     microsoft_path: 'https://graph.microsoft.com/v1.0/me'
-// }
-
 var config = {};
 
 config.google = {};
